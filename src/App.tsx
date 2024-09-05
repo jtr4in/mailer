5b987b581d739339e5ebdaa7d209c55fffdfe182
jtr4in <petrella@live.ca>
2024-09-04 22:44:18 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CampaignForm } from './components/CampaignForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Campaign Form</h1>
+      <CampaignForm />
+    </div>
+  );
+}
+
+export default App;
